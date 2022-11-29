b07f30da0a62f31463daf6784cea620d90dd9ad5
Peter B Norton <norton.peterb@gmail.com>
2022-11-29 13:13:44 +0000

    Read Data and Display on TFT as receieved

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,41 @@
 #include <Arduino.h>
+#include <TFT.h>
+#include <SPI.h>
+#include <Wire.h>
+
+#define CS 53
+#define DC 9
+#define RST 8
+
+TFT TFTscreen = TFT(CS,DC,RST);
+
+char buffer[96];
+
+void receiveEvent(int);
 
 void setup() {
-  
+  TFTscreen.begin();
+  TFTscreen.background(0x0000);
+  TFTscreen.stroke(0xFFFF);
+  TFTscreen.setTextSize(2);
+  TFTscreen.text("WAITING....",10,0);
+  Wire.begin(0x04);
+  Wire.onReceive(receiveEvent);
 }
 
 void loop() {
-  
+  delay(100);
+}
+
+void receiveEvent(int howMany) {
+  int count = 0;
+  while(0 < Wire.available()) {
+    buffer[count] = Wire.read();
+    count++;
+  }
+  buffer[count] = 0x00;
+  TFTscreen.background(0x0000);
+  TFTscreen.stroke(0xFFFF);
+  TFTscreen.setTextSize(2);
+  TFTscreen.text(buffer,10,0);
 }
